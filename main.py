8c68d33e32856661d8f98aa93bcd325022f94ccf
raulomiranda <raulomiranda@hotmail.com>
2025-10-03 18:30:22 -0300

    Add .gitignore, update main.py for MongoDB connection, and modify requirements.txt

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 from flask import Flask, jsonify
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+db_username = os.getenv("db_username")
+db_password = os.getenv("db_password")
+uri = f"mongodb+srv://{db_username}:{db_password}@metodoht.qe0n0od.mongodb.net/?retryWrites=true&w=majority&appName=MetodoHT"
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+
 
 app = Flask(__name__)
 
-# Seus dados de estatística
-estatisticas_futebol = {
-    "gols_por_jogo": 2.7,
-    "vitorias_casa": 45,
-    "vitorias_fora": 35
-}
-
 @app.route("/")
 def home():
-    return "Bem-vindo ao servidor de estatísticas de futebol!"
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+    return e
 
-@app.route("/estatisticas")
-def obter_estatisticas():
-    # Retorna os dados em JSON, prontos para serem consumidos pelo seu site
-    return jsonify(estatisticas_futebol)
 
-# ... mais rotas para times, jogadores, etc.
+
+@app.route("/inserir_time", methods=["POST"])
+def inserir_time_mongo():
+    # Lógica para inserir um time no MongoDB
+    return jsonify({"message": "Time inserido com sucesso!"}), 201
+
+#listar o times e ligas no mongo db 
 
 
 if __name__ == "__main__":
